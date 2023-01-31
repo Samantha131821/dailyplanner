2a4b863fa6cbd816308a3ed981537598f0df794b
Samantha131821 <samanthaleecamerlengo@gmail.com>
2023-01-31 11:50:04 -0500

    updated JS

--- Develop/script.js
+++ Develop/script.js
@@ -8,4 +8,17 @@ $(function () {
   var container = $('#container');
   var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-};
+  for (let i = 0; i < workHours.length; i++) {
+    
+    let row = $("<div class='row time-block'>")
+    let colHr = $("<div class='col-4 col-lg hour text-center'>")
+
+    let usaHrs = workHours[i] + ' a.m.'
+    if(workHours[i] > 12){
+      usaHrs = workHours[i] + ' p.m.'
+      if(workHours[i] >= 13){
+        usaHrs = workHours[i] - 12 + ' p.m.'
+      }
+    }
+  }
+})
