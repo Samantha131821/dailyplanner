85272056ebd71da0a8909a45cad51548b1d53c85
Samantha131821 <samanthaleecamerlengo@gmail.com>
2023-01-31 12:20:16 -0500

    updated ReadMe and final JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,3 @@
-
 $(function () {
   var today = dayjs();
   var currentDayEl = $('#currentDay');
@@ -7,18 +6,55 @@ $(function () {
 
   var container = $('#container');
   var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-
+//loop through work hours to determine AM or PM
   for (let i = 0; i < workHours.length; i++) {
     
     let row = $("<div class='row time-block'>")
     let colHr = $("<div class='col-4 col-lg hour text-center'>")
 
-    let usaHrs = workHours[i] + ' a.m.'
-    if(workHours[i] > 12){
-      usaHrs = workHours[i] + ' p.m.'
+    let usaHrs = workHours[i] + ' A.M.'
+    if(workHours[i] > 11){
+      usaHrs = workHours[i] + ' P.M.'
       if(workHours[i] >= 13){
-        usaHrs = workHours[i] - 12 + ' p.m.'
+        usaHrs = workHours[i] - 12 + ' P.M.'
       }
     }
+    colHr.append(usaHrs)
+
+//add classes and IDs from HTML and append
+    let textarea = $("<textarea class='col-6 col-md-10 description'>")
+    textarea.attr('id', `textarea-${i}`)
+
+// compare to current hour to determine past present or future
+    if(currentHour > workHours[i]){
+      textarea.addClass('past')
+    }
+    else if( currentHour === workHours[i]){
+      textarea.addClass('present')
+    }
+    else{
+      textarea.addClass('future')
+    }
+
+    let getTextArea = localStorage.getItem(`toDo-${i}`)
+    textarea.text(getTextArea)
+
+    let button = $("<button class='btn saveBtn col-2 col-md-1'>")
+    let btnIcon = $("<i class='fas fa-save'>")
+    button.append(btnIcon)
+
+
+
+    row.append(colHr, textarea, button)
+    container.append(row)
   }
+// on click save text to local storage
+  $('.saveBtn').on('click', function() {
+    for (let j = 0; j < workHours.length; j++) {
+      let textAreaValue = $(`#textarea-${j}`).val()
+      localStorage.setItem(`toDo-${j}`, textAreaValue)
+    }
+  })
 })
+  
+
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dailyplanner
+
+The purpose of this assignment was to create a daily work day planner. 
+
+The date should be displayed at the top of the screen.
+
+You should be able to determine past, present, or future time and compare it to current time. These should differentiated by different colors.
+
+User should be able to type in text areas and save info typed to local storage. 
+
+![Image](/Capture.PNG)
+
+The link to my planner is:
